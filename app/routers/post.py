8785ed8bfeaf5ce37db264bd2e7abf4343f12b6c
shyamyado <shyamyado@gmail.com>
2024-03-16 23:54:06 +0530

    Pagination

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
 from app import oauth2
 
@@ -12,21 +12,39 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(
-    db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)
+    db: Session = Depends(get_db),
+    current_user=Depends(oauth2.get_current_user),
+    limit: int = 10,
+    skip: int = 0,
+    search: Optional[str] = "",
 ):
     # cursor.execute("""SELECT * from posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
+    print(limit)
+    posts = (
+        db.query(models.Post)
+        .filter(models.Post.title.contains(search))
+        .limit(limit)
+        .offset(skip)
+        .all()
+    )
     return posts
 
 
 @router.get("/myposts", response_model=List[schemas.Post])
 def get_posts(
-    db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)
+    db: Session = Depends(get_db),
+    current_user=Depends(oauth2.get_current_user),
+    limit: int = 10,
 ):
     # cursor.execute("""SELECT * from posts""")
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    posts = (
+        db.query(models.Post)
+        .filter(models.Post.owner_id == current_user.id)
+        .limit(limit)
+        .all()
+    )
     return posts
 
 
